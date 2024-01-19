5d72d535f5e233d70cce28d142c7bbef24b75ace
HaiderMalik12 <haidermalik504@gmail.com>
2024-01-20 01:37:59 +0500

    remove comments

--- src/handlers/weather.ts
+++ src/handlers/weather.ts
@@ -2,17 +2,11 @@ import { validationResult } from 'express-validator';
 import fetch from 'node-fetch';
 
 export const getWeather = async (req, res) => {
-  console.log(req.params.location);
-
-  //make the api calls to weather api
-  //   console.log(process.env.WEATHER_API_KEY);
-
-  // fetch weather details
+  // TODO: validation on location
   const results = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${req.params.location}&aqi=no`
   );
 
-  //   //fetch the next forecast view for the day
   const weather = await results.json();
 
   return res.json({
