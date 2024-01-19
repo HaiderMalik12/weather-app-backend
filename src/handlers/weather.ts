4130854207cc0a88a1ca2e28fb184a4479ac5eff
HaiderMalik12 <haidermalik504@gmail.com>
2024-01-20 00:24:42 +0500

    completed mock

--- src/handlers/weather.ts
+++ src/handlers/weather.ts
@@ -2,6 +2,13 @@ import { validationResult } from 'express-validator';
 
 export const getWeather = async (req, res) => {
   console.log(req.params.location);
+
+  //make the api calls to weather api
+
+  // fetch weather details
+
+  //fetch the next forecast view for the day
+
   return res.json({
     city: 'Faisalabad',
     date: '01/19/24',
