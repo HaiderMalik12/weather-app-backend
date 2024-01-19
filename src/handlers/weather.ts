dee537c69eefca89c341d6bced5f4c55e4f5e960
HaiderMalik12 <haidermalik504@gmail.com>
2024-01-20 01:50:26 +0500

    change the api url from current to forecast

--- src/handlers/weather.ts
+++ src/handlers/weather.ts
@@ -4,7 +4,8 @@ import fetch from 'node-fetch';
 export const getWeather = async (req, res) => {
   // TODO: validation on location
   const results = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${req.params.location}&aqi=no`
+    // https://api.weatherapi.com/v1/forecast.json?q=Faisalabad&days=7&key=1ae613db94d24e42819185935241901
+    `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${req.params.location}&aqi=no&days=7`
   );
 
   const weather = await results.json();
