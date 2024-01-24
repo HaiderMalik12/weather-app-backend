ba5973eefa5917fd3bccb2b62f9cc153fd42f9c7
HaiderMalik12 <haidermalik504@gmail.com>
2024-01-25 02:09:53 +0500

    setup port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from './server';
-
+const PORT = process.env.PORT || 3001;
 app.listen(process.env.PORT, () => {
-  console.log('Server is running at port 3001');
+  console.log(`Server is running at port ${PORT}`);
 });
